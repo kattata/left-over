56498c8514c7177e5f83662650be0a0cfbee33de
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-12-08 00:23:10 +0100

    tailwind purge added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.html", "./**/*.html", "./src/**/*.vue", "./src/**/*.jsx", "./src/styles/scss/*.scss", "./public/styles/*.css"],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -92,17 +92,13 @@ module.exports = {
     backgroundImage: {
       none: "none",
       "gradient-to-t": "linear-gradient(to top, var(--tw-gradient-stops))",
-      "gradient-to-tr":
-        "linear-gradient(to top right, var(--tw-gradient-stops))",
+      "gradient-to-tr": "linear-gradient(to top right, var(--tw-gradient-stops))",
       "gradient-to-r": "linear-gradient(to right, var(--tw-gradient-stops))",
-      "gradient-to-br":
-        "linear-gradient(to bottom right, var(--tw-gradient-stops))",
+      "gradient-to-br": "linear-gradient(to bottom right, var(--tw-gradient-stops))",
       "gradient-to-b": "linear-gradient(to bottom, var(--tw-gradient-stops))",
-      "gradient-to-bl":
-        "linear-gradient(to bottom left, var(--tw-gradient-stops))",
+      "gradient-to-bl": "linear-gradient(to bottom left, var(--tw-gradient-stops))",
       "gradient-to-l": "linear-gradient(to left, var(--tw-gradient-stops))",
-      "gradient-to-tl":
-        "linear-gradient(to top left, var(--tw-gradient-stops))",
+      "gradient-to-tl": "linear-gradient(to top left, var(--tw-gradient-stops))",
     },
     backgroundOpacity: (theme) => theme("opacity"),
     backgroundPosition: {
@@ -171,8 +167,7 @@ module.exports = {
     },
     boxShadow: {
       sm: "0 1px 2px 0 rgba(0, 0, 0, 0.05)",
-      DEFAULT:
-        "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+      DEFAULT: "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
       md: "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
       lg: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
       xl: "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
@@ -214,10 +209,7 @@ module.exports = {
     divideWidth: (theme) => theme("borderWidth"),
     dropShadow: {
       sm: "0 1px 1px rgba(0,0,0,0.05)",
-      DEFAULT: [
-        "0 1px 2px rgba(0, 0, 0, 0.1)",
-        "0 1px 1px rgba(0, 0, 0, 0.06)",
-      ],
+      DEFAULT: ["0 1px 2px rgba(0, 0, 0, 0.1)", "0 1px 1px rgba(0, 0, 0, 0.06)"],
       md: ["0 4px 3px rgba(0, 0, 0, 0.07)", "0 2px 2px rgba(0, 0, 0, 0.06)"],
       lg: ["0 10px 8px rgba(0, 0, 0, 0.04)", "0 4px 3px rgba(0, 0, 0, 0.1)"],
       xl: ["0 20px 13px rgba(0, 0, 0, 0.03)", "0 8px 5px rgba(0, 0, 0, 0.08)"],
@@ -280,24 +272,8 @@ module.exports = {
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
       ],
-      serif: [
-        "ui-serif",
-        "Georgia",
-        "Cambria",
-        '"Times New Roman"',
-        "Times",
-        "serif",
-      ],
-      mono: [
-        "ui-monospace",
-        "SFMono-Regular",
-        "Menlo",
-        "Monaco",
-        "Consolas",
-        '"Liberation Mono"',
-        '"Courier New"',
-        "monospace",
-      ],
+      serif: ["ui-serif", "Georgia", "Cambria", '"Times New Roman"', "Times", "serif"],
+      mono: ["ui-monospace", "SFMono-Regular", "Menlo", "Monaco", "Consolas", '"Liberation Mono"', '"Courier New"', "monospace"],
     },
     fontSize: {
       xs: ["0.75rem", { lineHeight: "1rem" }],
@@ -755,8 +731,7 @@ module.exports = {
     transitionProperty: {
       none: "none",
       all: "all",
-      DEFAULT:
-        "background-color, border-color, color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter",
+      DEFAULT: "background-color, border-color, color, fill, stroke, opacity, box-shadow, transform, filter, backdrop-filter",
       colors: "background-color, border-color, color, fill, stroke",
       opacity: "opacity",
       shadow: "box-shadow",
@@ -869,45 +844,17 @@ module.exports = {
     backgroundAttachment: ["responsive"],
     backgroundBlendMode: ["responsive"],
     backgroundClip: ["responsive"],
-    backgroundColor: [
-      "responsive",
-      "dark",
-      "group-hover",
-      "focus-within",
-      "hover",
-      "focus",
-    ],
+    backgroundColor: ["responsive", "dark", "group-hover", "focus-within", "hover", "focus"],
     backgroundImage: ["responsive"],
-    backgroundOpacity: [
-      "responsive",
-      "dark",
-      "group-hover",
-      "focus-within",
-      "hover",
-      "focus",
-    ],
+    backgroundOpacity: ["responsive", "dark", "group-hover", "focus-within", "hover", "focus"],
     backgroundPosition: ["responsive"],
     backgroundRepeat: ["responsive"],
     backgroundSize: ["responsive"],
     backgroundOrigin: ["responsive"],
     blur: ["responsive"],
     borderCollapse: ["responsive"],
-    borderColor: [
-      "responsive",
-      "dark",
-      "group-hover",
-      "focus-within",
-      "hover",
-      "focus",
-    ],
-    borderOpacity: [
-      "responsive",
-      "dark",
-      "group-hover",
-      "focus-within",
-      "hover",
-      "focus",
-    ],
+    borderColor: ["responsive", "dark", "group-hover", "focus-within", "hover", "focus"],
+    borderOpacity: ["responsive", "dark", "group-hover", "focus-within", "hover", "focus"],
     borderRadius: ["responsive"],
     borderStyle: ["responsive"],
     borderWidth: ["responsive"],
@@ -1002,29 +949,9 @@ module.exports = {
     strokeWidth: ["responsive"],
     tableLayout: ["responsive"],
     textAlign: ["responsive"],
-    textColor: [
-      "responsive",
-      "dark",
-      "group-hover",
-      "focus-within",
-      "hover",
-      "focus",
-    ],
-    textDecoration: [
-      "responsive",
-      "group-hover",
-      "focus-within",
-      "hover",
-      "focus",
-    ],
-    textOpacity: [
-      "responsive",
-      "dark",
-      "group-hover",
-      "focus-within",
-      "hover",
-      "focus",
-    ],
+    textColor: ["responsive", "dark", "group-hover", "focus-within", "hover", "focus"],
+    textDecoration: ["responsive", "group-hover", "focus-within", "hover", "focus"],
+    textOpacity: ["responsive", "dark", "group-hover", "focus-within", "hover", "focus"],
     textOverflow: ["responsive"],
     textTransform: ["responsive"],
     transform: ["responsive"],
