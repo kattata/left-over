087904ec6f4576ca524b72fc0962ad34618f45a7
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-12-09 09:46:16 +0100

    fd

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -29,25 +29,31 @@ function appendPosts(posts) {
   for (post of posts) {
     htnlTemplate = `
     <article class="post-box border-2 mb-4 border-light-black rounded-3xl overflow-hidden">
-    <img src="./src//media//posted/${post.image_name}" alt="image of sold food" />
+    <img src="./src/media/posted/${post.image_name}" alt="image of sold food" />
     <div class="post-content-wrapper mx-3">
       <div class="flex justify-between mt-2">
         <span class="food-category-badge 
-        ${post.category == "Fruits & Vegetables" ? "bg-light-green-custom" : ""} ${post.category == "Dish" ? "bg-light-blue" : ""}
-        ${post.category == "Bread & Pastry" ? "bg-light-orange" : ""} ${post.category == "Dessert" ? "bg-light-violet" : ""}
-        ${post.category == "Diary" ? "bg-light-red" : ""}">${post.category}</span>
+        ${
+          post.category == "Fruits & Vegetables" ? "bg-light-green-custom" : ""
+        } ${post.category == "Dish" ? "bg-light-blue" : ""}
+        ${post.category == "Bread & Pastry" ? "bg-light-orange" : ""} ${
+      post.category == "Dessert" ? "bg-light-violet" : ""
+    }
+        ${post.category == "Diary" ? "bg-light-red" : ""}">${
+      post.category
+    }</span>
         <div class="flex">
-          <img src="./src/media/posted/avatar-test.png" alt="" />
+          <img class=" pr-1" src="./src/media/posted/avatar-test.png" alt="" />
           <p>Piotr Pospiech</p>
         </div>
       </div>
       <div class="flex justify-between font-bold mt-4">
         <p>${post.product_name}</p>
-        <p>Dkk ${post.price}</p>
+        <p>DKK ${post.price}</p>
       </div>
       <div class="flex justify-between mt-1 mb-4 opacity-50 text-xs">
-        <p>Dkk ${post.amount}</p>
-        <p>Expires:${post.expires_in}</p>
+        <p>Amount ${post.amount}</p>
+        <p>Expires: ${post.expires_in}</p>
       </div>
     </div>
   </article>
