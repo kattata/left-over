146be6b380be5fcb559f93edf9d4a166d130042e
Kasia Laniecka <kl@parkshare.dk>
2021-12-10 17:59:04 +0100

    started edit profile

--- src/backend/updateUser.php
+++ src/backend/updateUser.php
@@ -6,18 +6,31 @@ require('mysql.php');
 $db = new MySQL();
 $db->Connect();
 
+// VALIDATION
+function allFieldsFilledEdit($username, $email, $phoneNumber, $address, $zipCode, $city, $img) {
+    if($username && $email && $phoneNumber && $address && $zipCode && $city && $img) {
+        return true;
+    } else {
+        global $error;
+        $error = "Fill out all fields";
+        return false;
+    }
+}
+
 if ($_GET['action'] == 'updateUser') {
     // get user from frontend
     $newUser = json_decode(file_get_contents("php://input"));
+    $id = $newUser->id;
     $username = $newUser->name;
     $email = $newUser->email;
     $phoneNumber = $newUser->phoneNumber;
     $address = $newUser->address;
     $zipCode = $newUser->zipCode;
     $city = $newUser->city;
+    $img = $newUser->img;
 
     // validation
-    if(allFieldsFilledEdit($username, $email, $phoneNumber, $address, $zipCode, $city)) {
+    if(allFieldsFilledEdit($username, $email, $phoneNumber, $address, $zipCode, $city, $img)) {
         global $db;
         $results = $db->Query("UPDATE users SET 
         username = '$username',
@@ -25,8 +38,9 @@ if ($_GET['action'] == 'updateUser') {
         phone_number = '$phoneNumber',
         address = '$address',
         zip_code = '$zipCode',
-        city = '$city' 
-        WHERE email = '$email'");
+        city = '$city'
+        image_name = '$img' 
+        WHERE user_id = '$id'");
         global $error;
         $error = "";
         echo json_encode($error);
--- src/frontend/editProfile.js
+++ src/frontend/editProfile.js
@@ -1,5 +1,3 @@
-// const baseUrl = "http://localhost:3000/test/backend.php";
-
 const editProfileUsername = document.querySelector(".edit-profile-name");
 const editProfileEmail = document.querySelector(".edit-profile-email");
 const editProfilePassword = document.querySelector(".edit-profile-password");
@@ -12,23 +10,35 @@ const editProfilePhoneNumber = document.querySelector(
 const editProfileAddress = document.querySelector(".edit-profile-address");
 const editProfileZipCode = document.querySelector(".edit-profile-zip-code");
 const editProfileCity = document.querySelector(".edit-profile-city");
+const editProfileImg = document.querySelector(".edit-profile-picture");
 
 let editProfileError = "";
+let uploadedEditImgName = "";
+
+editProfileImg.addEventListener("change", (e) => {
+  uploadedEditImgName = e.target.files[0].name;
+});
 
 async function updateUser() {
+  const currentUserId = JSON.parse(sessionStorage.getItem("user")).user_id;
   const user = {
+    id: currentUserId,
     name: editProfileUsername.value,
     email: editProfileEmail.value,
     phoneNumber: editProfilePhoneNumber.value,
     address: editProfileAddress.value,
     zipCode: editProfileZipCode.value,
     city: editProfileCity.value,
+    img: uploadedEditImgName,
   };
-  const response = await fetch(baseUrl + "?action=updateUser", {
-    method: "POST",
-    headers: { "Content-Type": "application/json; charset=utf-8" },
-    body: JSON.stringify(user),
-  });
+  const response = await fetch(
+    "http://localhost:3000/src/backend/createUser.php?action=updateUser",
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      body: JSON.stringify(user),
+    }
+  );
   const result = await response.json();
   console.log(result);
   editProfileError = result;
