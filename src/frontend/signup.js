e69dd4b677f4450b7fdaaad895c705c6416b2869
Kasia Laniecka <kl@parkshare.dk>
2021-12-11 13:50:11 +0100

    signup with upload image

--- src/frontend/signup.js
+++ src/frontend/signup.js
@@ -10,12 +10,25 @@ const signupImg = document.querySelector(".signup-picture");
 
 let signupError = "";
 let uploadedImgName = "";
+let uploadedImg = "";
 
 signupImg.addEventListener("change", (e) => {
   uploadedImgName = e.target.files[0].name;
+  uploadedImg = e.target.files[0];
 });
 
 async function createUser() {
+  const formData = new FormData();
+  formData.append("file", uploadedImg);
+  formData.append("name", signupUsername.value);
+  formData.append("email", signupEmail.value);
+  formData.append("password", signupPassword.value);
+  formData.append("rptPassword", signupRptPassword.value);
+  formData.append("phoneNumber", signupPhoneNumber.value);
+  formData.append("address", signupAddress.value);
+  formData.append("zipCode", signupZipCode.value);
+  formData.append("city", signupCity.value);
+
   const newUser = {
     name: signupUsername.value,
     email: signupEmail.value,
@@ -27,12 +40,20 @@ async function createUser() {
     city: signupCity.value,
     img: uploadedImgName,
   };
+
+  // const response = await fetch(
+  //   "http://localhost:3000/src/backend/createUser.php?action=createUser",
+  //   {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json; charset=utf-8" },
+  //     body: JSON.stringify(newUser),
+  //   }
+  // );
   const response = await fetch(
     "http://localhost:3000/src/backend/createUser.php?action=createUser",
     {
       method: "POST",
-      headers: { "Content-Type": "application/json; charset=utf-8" },
-      body: JSON.stringify(newUser),
+      body: formData,
     }
   );
   const result = await response.json();
