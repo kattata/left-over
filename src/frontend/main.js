d2327f278f52a79777999770cac0c5203fd8588c
Piotr Pospiech <55554468+Piotr20@users.noreply.github.com>
2021-12-14 11:38:47 +0100

    functionality added

--- src/frontend/main.js
+++ src/frontend/main.js
@@ -18,13 +18,21 @@ function appendPosts(posts) {
   for (let post of posts) {
     htnlTemplate = `
     <article class="post-box border-2 mb-4 border-light-black rounded-3xl overflow-hidden">
-    <img class="max-h-24 w-full object-cover" src="./src/media/posted/${post.image_name}" alt="image of sold food" />
+    <img class="max-h-24 w-full object-cover" src="./src/media/posted/${
+      post.image_name
+    }" alt="image of sold food" />
     <div class="post-content-wrapper mx-3">
       <div class="flex justify-between mt-2">
         <span class="food-category-badge 
-        ${post.category == "Fruits & Vegetables" ? "bg-light-green-custom" : ""} ${post.category == "Dish" ? "bg-light-blue" : ""}
-        ${post.category == "Bread & Pastry" ? "bg-light-orange" : ""} ${post.category == "Dessert" ? "bg-light-violet" : ""}
-        ${post.category == "Diary" ? "bg-light-red" : ""}">${post.category}</span>
+        ${
+          post.category == "Fruits & Vegetables" ? "bg-light-green-custom" : ""
+        } ${post.category == "Dish" ? "bg-light-blue" : ""}
+        ${post.category == "Bread & Pastry" ? "bg-light-orange" : ""} ${
+      post.category == "Dessert" ? "bg-light-violet" : ""
+    }
+        ${post.category == "Diary" ? "bg-light-red" : ""}">${
+      post.category
+    }</span>
         <div class="flex">
           <img class=" pr-1" src="./src/media/posted/avatar-test.png" alt="" />
           <p>Piotr Pospiech</p>
@@ -67,87 +75,141 @@ init();
 
 // SPLASH SCREEN
 const splash = document.querySelector(".splash");
-document.addEventListener("DOMContentLoaded", (e)=>{
-  setTimeout(()=>{
+document.addEventListener("DOMContentLoaded", (e) => {
+  setTimeout(() => {
     splash.classList.add("display-none");
   }, 1000);
-})
+});
 
+// class Item {
+// 	constructor (itemName, itemAmount) {
+// 		this.name = itemName;
+//     this.amount = itemAmount;
+// 		this.create();
+// 	}
+
+// create () {
+// let html = "";
+// for (const userObject of usersArray) {
+//     html += /*html*/`
+//         <article>
+//             <img src="${userObject.avatarUrl}" onclick="showDetailView('${userObject.id}')">
+//             <h2>${userObject.name}</h2>
+//             <a href="mailto:${userObject.email}">${userObject.email}</a>
+//             <p>${userObject.enrollmentType.replace("Enrollment", "")}</p>
+//             <p>Course: ${userObject.course}</p>
+//             <button onclick="selectUser('${userObject.id}')">Update</button>
+//             <button onclick="deleteUser('${userObject.id}')">Delete</button>
+//         </article>
+//     `;
+// }
+// document.querySelector("#users-container").innerHTML = html;
+
+// 		let listItem = document.createElement("div");
+// 		listItem.classList.add("list-item");
+
+//     let removeButton = document.createElement("input");
+// 		removeButton.classList.add("remove");
+//     removeButton.type = "radio";
+// 		removeButton.addEventListener("click", () => this.remove(listItem));
+
+// 		let input = document.createElement("div");
+// 		input.type = "text";
+// 		input.classList.add("item-name");
+//     input.value = this.name;
+// 		input.disabled = true;
+
+//     let amountInput = document.createElement("input");
+//     amountInput.type = "text";
+//     amountInput.classList.add("item-amount");
+//     amountInput.value = this.amount;
+//     amountInput.disabled = true;
+
+// 		let actions = document.createElement("div");
+// 		actions.classList.add("item-actions");
+
+// 		actions.appendChild(removeButton);
+
+//     listItem.appendChild(actions);
+// 		listItem.appendChild(input);
+//     listItem.appendChild(amountInput);
+// 		itemList.appendChild(listItem);
+// 	}
+
+// 	remove (listItem) {
+// 		listItem.parentNode.removeChild(listItem);
+// 	}
+// }
+
+// function newItem () {
+// 	if (itemToAdd.value != "" || productAmount.value != "") {
+// 		new Item(itemToAdd.value, productAmount.value);
+// 		itemToAdd.value = "";
+//     productAmount.value = "";
+// 	}
+// }
 // SHOPING LIST
 const itemToAdd = document.getElementById("itemToAdd");
 const productAmount = document.getElementById("productAmount");
+const productUnit = document.getElementById("productUnit");
 const addButton = document.getElementById("addButton");
 const itemList = document.getElementById("list");
 
-class Item {
-	constructor (itemName, itemAmount) {
-		this.name = itemName;
-    this.amount = itemAmount;
-		this.create();
-	}
+let shoppingListItems = [];
 
-	create () {
-    // let html = "";
-    // for (const userObject of usersArray) {
-    //     html += /*html*/`
-    //         <article>
-    //             <img src="${userObject.avatarUrl}" onclick="showDetailView('${userObject.id}')">
-    //             <h2>${userObject.name}</h2>
-    //             <a href="mailto:${userObject.email}">${userObject.email}</a>
-    //             <p>${userObject.enrollmentType.replace("Enrollment", "")}</p>
-    //             <p>Course: ${userObject.course}</p>
-    //             <button onclick="selectUser('${userObject.id}')">Update</button>
-    //             <button onclick="deleteUser('${userObject.id}')">Delete</button>
-    //         </article>
-    //     `;
-    // }
-    // document.querySelector("#users-container").innerHTML = html;
-
-		let listItem = document.createElement("div");
-		listItem.classList.add("list-item");
-
-    let removeButton = document.createElement("input");
-		removeButton.classList.add("remove");
-    removeButton.type = "radio";
-		removeButton.addEventListener("click", () => this.remove(listItem));
-
-		let input = document.createElement("div");
-		input.type = "text";
-		input.classList.add("item-name");
-    input.value = this.name;
-		input.disabled = true;
-
-    let amountInput = document.createElement("input");
-    amountInput.type = "text";
-    amountInput.classList.add("item-amount");
-    amountInput.value = this.amount;
-    amountInput.disabled = true;
-
-		let actions = document.createElement("div");
-		actions.classList.add("item-actions");
-
-		actions.appendChild(removeButton);
-    
-    listItem.appendChild(actions);
-		listItem.appendChild(input);
-    listItem.appendChild(amountInput);
-		itemList.appendChild(listItem);
-	}
-
-	remove (listItem) {
-		listItem.parentNode.removeChild(listItem);
-	}
+function addShoppingListItem() {
+  itemList.innerHTML = "";
+  let htmlShoppingListTemplate = ``;
+  shoppingListItems.push({
+    itemId: Date.now(),
+    productName: itemToAdd.value,
+    productAmount: productAmount.value,
+    productUnit: productUnit.value,
+  });
+  for (const shoppingListItem of shoppingListItems) {
+    htmlShoppingListTemplate = `
+      <div class="flex items-center justify-between form-input mb-3" >
+        <div class=" flex items-center">
+          <input onClick="removeItem(${shoppingListItem.itemId})" class=" mr-2" type="radio" name="discard-item">
+          <p>${shoppingListItem.productName}</p>
+        </div>
+        <p>${shoppingListItem.productAmount} ${shoppingListItem.productUnit}</p>
+      </div>
+      
+    `;
+    itemList.innerHTML += htmlShoppingListTemplate;
+  }
+}
+function removeItem(clickedItemId) {
+  console.log(clickedItemId);
+  for (let index = 0; index < shoppingListItems.length; index++) {
+    const element = shoppingListItems[index];
+    if (element.itemId == clickedItemId) {
+      shoppingListItems.splice(index, 1);
+      updateList(shoppingListItems);
+    }
+  }
 }
 
-function newItem () {
-	if (itemToAdd.value != "" || productAmount.value != "") {
-		new Item(itemToAdd.value, productAmount.value);
-		itemToAdd.value = "";
-    productAmount.value = "";
-	}
+function updateList(updatedShoppingList) {
+  itemList.innerHTML = "";
+  let htmlShoppingListTemplate = ``;
+  for (const shoppingListItem of updatedShoppingList) {
+    htmlShoppingListTemplate = `
+      <div class="flex items-center justify-between form-input mb-3" >
+        <div class=" flex items-center">
+          <input onClick="removeItem(${shoppingListItem.itemId})" class=" mr-2" type="radio" name="discard-item">
+          <p>${shoppingListItem.productName}</p>
+        </div>
+        <p>${shoppingListItem.productAmount} ${shoppingListItem.productUnit}</p>
+      </div>
+      
+    `;
+    itemList.innerHTML += htmlShoppingListTemplate;
+  }
 }
 
 document.querySelector(".form-shopping").addEventListener("submit", (e) => {
   e.preventDefault();
-  newItem();
-})
+  addShoppingListItem();
+});
